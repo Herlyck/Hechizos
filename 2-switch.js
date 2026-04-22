b217f7babf22420b224c9783065344776e6f13d4
Herlyck <mrcyrax64@gmail.com>
2026-04-22 19:25:46 -0300

    Add files via upload

--- 2-switch.js
+++ 2-switch.js
@@ -1,44 +1,46 @@
 const leer = require("prompt-sync")();
+const DETALLE_NUCLEO= "***Detalles del nucleo [";
+
+
 function main(params) {
-    let nucleoVarita=0;
+    let nucleoVarita = 0;
+    
 
     console.log("escriba el nucleo de varita(0-1-2):");
-    nucleoVarita=Number(leer());
+    nucleoVarita = Number(leer());
+    
+    if (nucleoVarita <= 2 && nucleoVarita>= 0 && !isNaN(nucleoVarita)) {
+        process.stdout.write(DETALLE_NUCLEO + nucleoVarita);
+    }
 
     switch (nucleoVarita) {
         case 0:
-            console.log("***Detalles del nucleo [0] Fenix***");
-            console.log("Fenix - ¡Excelente elección! Las varitas con núcleo de fénix son poderosas y leales") 
-            console.log("***Fin del Mensaje***");
-            console.log("-----------------------------");
-           
-            
+            console.log("] Fenix***");
             break;
         case 1:
-            console.log("***Detalles del nucleo [1] Unicornio***");
-            console.log("Unicornio - Las varitas con núcleo de unicornio son conocidas por su pureza y fuerza.");
-            console.log("***Fin del Mensaje***");
-            console.log("-----------------------------");
-            
+            console.log("] Unicornio***");
             break;
         case 2:
-            console.log("***Detalles del nucleo [2] Dragon***");
+            console.log("] Dragon***");
+            break;
+        
+    }
+
+    switch (nucleoVarita) {
+        case 0:
+            console.log("Fenix - ¡Excelente elección! Las varitas con núcleo de fénix son poderosas y leales")
+            break;
+        case 1:
+            console.log("Unicornio - Las varitas con núcleo de unicornio son conocidas por su pureza y fuerza.");
+            break;
+        case 2:
             console.log("Dragón - ¡Cuidado! Las varitas con núcleo de dragón son poderosas pero pueden ser temperamentales.");
-            console.log("***Fin del Mensaje***");
-            console.log("-----------------------------");
-            
             break;
         default:
-            console.log("***Detalles del nucleo [2] Dragon***");
             console.log("No reconocemos ese tipo de núcleo. Por favor, ingrese un núcleo válido");
-            console.log("***Fin del Mensaje***");
-            console.log("-----------------------------");
-            
             break;
     }
-    
-
-
-
+    console.log("***Fin del Mensaje***");
+    console.log("-----------------------------");
 }
 main();
